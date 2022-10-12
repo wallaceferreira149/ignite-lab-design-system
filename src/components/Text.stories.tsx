e2ac63e96c724394f3abd9758898f8821e365687
wallaceferreira149 <wallaceferreira149@gmail.com>
2022-10-12 19:17:22 -0400

    intial commit

---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,51 @@
+import {Meta, StoryObj} from '@storybook/react'
+import {Text, TextProps} from './Text'
+
+export default {
+  title: 'Components/Text',
+  component: Text,
+  args: {
+    children: 'normal text',
+  },
+  argTypes: {
+    size: {
+      options: ['sm', 'md', 'lg'],
+      control: {
+        type: 'inline-radio'
+      }
+    }
+  }
+} as Meta<TextProps>;
+
+export const Default: StoryObj = {}
+export const Small: StoryObj<TextProps> = {
+  args: {
+    size: 'sm'
+  }
+}
+export const Large: StoryObj<TextProps> = {
+   args: {
+    size: 'lg'
+   }
+}
+
+export const CustomComponent: StoryObj<TextProps> = {
+   args: {
+    asChild: true,
+    children: (
+      <p>as paragraph</p>
+    )
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      }
+    },
+    asChild: {
+      table: {
+        disable: true,
+      }
+    }
+  }
+}
